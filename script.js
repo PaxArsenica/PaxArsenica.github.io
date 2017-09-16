0e1282705a679e188c69733f8b9981af0875238b
Arsene Lakpa <aclak1@yahoo.com>
2017-09-16 15:35:56 -0400

    Initial commit.

---
+++ script.js
@@ -0,0 +1,18 @@
+var fancyText = document.getElementById('fancy');
+var intervalTime = 150;
+var initialPause = 1000;
+var callbackPause = 500;
+
+function deleteContent(callback) {
+
+    var intervalId = setInterval(function() {
+        if (fancyText.innerHTML.length == 0) {
+            clearInterval(intervalId);
+
+            if (callback) {
+                setTimeout(callback, callbackPause);
+            }
+        }
+
+        fancyText.innerHTML = fancyText.innerHTML.substring(0, fancyText.innerHTML.length - 1);
+    }, intervalTime);
